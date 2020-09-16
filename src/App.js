ee0150425f09ce6c429c57c3fd81514214ef0388
Tommy Nienchi Chen <tommynchen@gmail.com>
2020-09-15 21:50:27 -0700

    copypasta

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import HomePage from './HomePage/index'
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <header className="App-header">
+        <HomePage />
+      </header>
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/HomePage/data.js
@@ -0,0 +1,118 @@
+
+
+export const FootballData = [
+    {
+        title: "Browns vs Stealers",
+        teamOne: "Browns",
+        teamTwo: "Stealers",
+        teamOneOdds: "+130",
+        teamTwoOdds: "-140",
+        teamOneOver: "+7 +140",
+        teamTwoOver: "-7 -120",
+        liquidity: "7.14"
+    },
+    {
+        title: "Cavs vs Golden State",
+        teamOne: "Cavs",
+        teamTwo: "Golden State",
+        teamOneOdds: "+110",
+        teamTwoOdds: "-170",
+        teamOneOver: "+3 +120",
+        teamTwoOver: "-2 -150",
+        liquidity: "4.22"
+    },
+    {
+        title: "Jets vs Patroits",
+        teamOne: "Jets",
+        teamTwo: "Patroits",
+        teamOneOdds: "+180",
+        teamTwoOdds: "-140",
+        teamOneOver: "+10 +120",
+        teamTwoOver: "-5 -160",
+        liquidity: "6.74"
+    },
+    {
+        title: "Browns vs Stealer",
+        teamOne: "Browns",
+        teamTwo: "Patroits",
+        teamOneOdds: "+130",
+        teamTwoOdds: "-140",
+        teamOneOver: "+7 +140",
+        teamTwoOver: "-7 -120",
+        liquidity: "7.82"
+    }
+]
+
+export const MMAData = [
+    {
+        title: "Rua vs. Henderson",
+        teamOne: "Rua",
+        teamTwo: "Henderson",
+        teamOneOdds: "+110",
+        teamTwoOdds: "-170",
+        teamOneOver: "+1 +160",
+        teamTwoOver: "-2 -170",
+        liquidity: "8.92"
+    },
+    {
+        title: "Jones vs. Gustafsson",
+        teamOne: "Jones",
+        teamTwo: "Gustafsson",
+        teamOneOdds: "+150",
+        teamTwoOdds: "-130",
+        teamOneOver: "+4 +180",
+        teamTwoOver: "-1 -120",
+        liquidity: "7.14"
+    },
+    {
+        title: "Diaz vs. McGregor",
+        teamOne: "Diaz",
+        teamTwo: "McGregor",
+        teamOneOdds: "+105",
+        teamTwoOdds: "-110",
+        teamOneOver: "+3 +140",
+        teamTwoOver: "-1 -130",
+        liquidity: "9.23"
+    },
+    {
+        title: "Hunt vs. Silva",
+        teamOne: "Hunt",
+        teamTwo: "Silva",
+        teamOneOdds: "+180",
+        teamTwoOdds: "-120",
+        teamOneOver: "+5 +110",
+        teamTwoOver: "-5 -140",
+        liquidity: "4.52"
+    }
+]
+
+export const EsportsData = [
+    {
+        title: "Ninja vs Samurai",
+        teamOne: "+130",
+        teamTwo: "-140",
+        teamOneOver: "+7 +140",
+        teamTwoOver: "-7 -120"
+    },
+    {
+        title: "Cloud9 vs TSM",
+        teamOne: "+110",
+        teamTwo: "-170",
+        teamOneOver: "+3 +120",
+        teamTwoOver: "-2 -150"
+    },
+    {
+        title: "Immortals vs GG",
+        teamOne: "+180",
+        teamTwo: "-140",
+        teamOneOver: "+10 +120",
+        teamTwoOver: "-5 -160"
+    },
+    {
+        title: "G2 vs T1",
+        teamOne: "+130",
+        teamTwo: "-140",
+        teamOneOver: "+7 +140",
+        teamTwoOver: "-7 -120"
+    }
+]
